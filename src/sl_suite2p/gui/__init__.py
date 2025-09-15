c261b72d913e4ab4df744a348a8adee300717656
Katlynn Ryu <kmr238@cornell.edu>
2025-09-15 13:32:14 -0400

    refactor gui/__init__.py

--- src/sl_suite2p/gui/__init__.py
+++ src/sl_suite2p/gui/__init__.py
@@ -1,3 +1,11 @@
-"""Copyright © 2023 Howard Hughes Medical Institute, Authored by Carsen Stringer and Marius Pachitariu."""
+"""This package contains the interactive Suite2p GUI.
+
+It exposes the public entry point `run`, which launches the GUI.
+Copyright © 2023 Howard Hughes Medical Institute,
+Authored by Carsen Stringer and Marius Pachitariu.
+"""
+from __future__ import annotations
 
 from .gui2p import run
+
+__all__ = ["run"]
