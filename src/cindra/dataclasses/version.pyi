1a0d0688769d536d23ea57dc6847b36f2780642f
Ivan Kondratyev <ikondratv@gmail.com>
2026-08-08 15:36:16 -0400

    Removed the generated type stubs for the development cycle.

--- src/cindra/dataclasses/version.pyi
+++
@@ -1,5 +0,0 @@
-from importlib_metadata import PackageMetadata as PackageMetadata
-
-_package_metadata: PackageMetadata | None
-VERSION: str
-PYTHON_VERSION: str
--- src/cindra/extraction/__init__.pyi
+++
@@ -1,8 +0,0 @@
-from .masks import create_masks as create_masks
-from .extract import extract_traces as extract_traces
-from .deconvolve import (
-    apply_oasis_deconvolution as apply_oasis_deconvolution,
-    compute_delta_fluorescence as compute_delta_fluorescence,
-)
-
-__all__ = ["apply_oasis_deconvolution", "compute_delta_fluorescence", "create_masks", "extract_traces"]
